9e5d2efa409d28a8e027bcfed99ac2fb09a1fab3
TejShind <tej.gholap25@gmail.com>
2022-02-21 20:46:23 +0530

    [Tejaswini]ADDED:Feature for Welcome message

---
+++ src/com/blz/linecomparsion/LineComparsion.java
@@ -0,0 +1,8 @@
+package com.blz.linecomparsion;
+
+public class LineComparsion {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line comparsion Computation Program");
+    }
+}
