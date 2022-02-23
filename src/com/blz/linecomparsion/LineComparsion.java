98f4a905812e9c84539ee7dfef82bf4e43e17c94
TejShind <tej.gholap25@gmail.com>
2022-02-24 01:58:18 +0530

    [Tejaswini]ADDED:Feature for Calculate Line length

--- src/com/blz/linecomparsion/LineComparsion.java
+++ src/com/blz/linecomparsion/LineComparsion.java
@@ -1,8 +1,26 @@
 package com.blz.linecomparsion;
+import java.util.Scanner;
 
 public class LineComparsion {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Line comparsion Computation Program");
+        System.out.println("Enter the first line coordinates");
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter x1");
+        int x1 = input.nextInt();
+        System.out.println("enter y1");
+        int y1 = input.nextInt();
+        System.out.println("enter x2");
+        int x2 = input.nextInt();
+        System.out.println("enter y2");
+        int y2 = input.nextInt();
     }
+
+    public double length(int a, int b, int c, int d) {
+        double length0fline = Math.sqrt(Math.pow((c - a), 2) * Math.pow((d - b), 2));
+        return length0fline;
+    }
+
 }
+
+
