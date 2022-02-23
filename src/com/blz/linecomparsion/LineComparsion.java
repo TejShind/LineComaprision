d3533f9a02dfc7a845298ea0121f575289169357
TejShind <tej.gholap25@gmail.com>
2022-02-24 02:23:08 +0530

    [Tejaswini]ADDED:Feature for Compare Length

--- src/com/blz/linecomparsion/LineComparsion.java
+++ src/com/blz/linecomparsion/LineComparsion.java
@@ -35,6 +35,7 @@ public class LineComparsion {
         LineComparsion f = new LineComparsion();
     Double len2 = f.length(p1, q1, p2, q2);
 		System.out.println("Second Line length is : "+len2+"");
+        System.out.println(len1.equals(len2));
 
 }
 
